5dcdf3e55e88de28f9bf386681838d13ea9f078f
parsa011 <parsa32569@gmail.com>
2022-04-09 12:26:28 +0430

    [ngc.h] : create base header file for basic defs
    ngc.h is our base header file , we defile all basic definitins like
    public and private and other types here

---
+++ src/ngc.h
@@ -0,0 +1,18 @@
+#ifndef _NGC_H
+# define _NGC_H
+/*
+*	ngc programming language
+*	Copyright
+*		(C) 2022 Parsa Mahmoudy Sahebi
+*
+*	This file is part of ngc languge (new generation c)
+*/
+
+/*
+ *	These are some basic definitions, for readablity we define extern as public
+ *	and private as static
+ */
+#define public extern
+#define private static
+
+#endif
