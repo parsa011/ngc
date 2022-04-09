e651ca5978eb0702aba58b6f106bbb2ecfb59f7b
parsa011 <parsa32569@gmail.com>
2022-04-09 21:04:52 +0430

    [lexer.h] : Some Basic Definitions for lexer and function prototype

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -8,4 +8,44 @@
 *	This file is part of ngc languge (new generation c)
 */
 
+#include "../ngc.h"
+
+struct position {
+	int line;
+	int col;
+};
+
+struct lexer {
+	FILE *fp;
+	char *file_name;
+	char current_char;
+	struct position pos;
+	struct position prevpos;
+};
+
+/*
+ *	@brief : Return new char from given file, but If There isn't Anything Saved Char
+ */
+private char next_char();
+
+/*
+ *	@brief : Put back given Char, To get it after new new_char call
+ */
+private void put_back(int);
+
+/*
+ *	@brief : Skip White spaces And and put_back last non space char to give it from next_char
+ */
+private void skip_whitespace();
+
+/*
+ *	@brief : Eat One line Comment, skip until new \n :)
+ */
+private void skip_ol_comment();
+
+/*
+ *	@brief : Skipt Multiline comments
+ */
+private void skip_ml_comment();
+
 #endif
