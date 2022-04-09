916213056e0729f7a95ef049535c80d8b30416a6
parsa011 <parsa32569@gmail.com>
2022-04-09 19:13:26 +0430

    [token.h] : add some token and their string
    we store each token name in global array names : token_type_str

--- src/token/token.h
+++ src/token/token.h
@@ -8,6 +8,8 @@
 *	This file is part of ngc languge (new generation c)
 */
 
+#include "../ngc.h"
+
 enum {
 	T_EOF,
 
@@ -15,7 +17,7 @@ enum {
 	T_DASH,			// -
 	T_STAR,			// *
 	T_SLASH,		// /
-	T_PERCENT		// %
+	T_PERCENT,		// %
 	T_PIPE,			// |
 	T_PIPEPIPE,		// ||
 	T_EQUAL,		// =
@@ -92,12 +94,114 @@ enum {
 	T_DOUBLE,
 	T_FLOAT,
 	T_LONG,
-	T_STR,
 
-	T_INTLIT,
-	T_IDENT
+	T_STRLIT,		 // "string"
+	T_INTLIT,		 // 123
+	T_OCTALLIT,		 // 012
+	T_HEXLIT,		 // 0x123
+	T_BINLIT,		 //	0b123 
+	T_CHARLIT,		 // 'c'
+
+	T_IDENT,
 
 	T_BAD
 } token_type;
+
+char *token_type_str[] = {
+	"T_EOF",
+
+	"T_PLUS", 		// +
+	"T_DASH",		// -
+	"T_STAR",		// *
+	"T_SLASH",		// /
+	"T_PERCENT,"	// %
+	"T_PIPE,"		// |
+	"T_PIPEPIPE",	// ||
+	"T_EQUAL",		// =
+	"T_AND",		// &
+	"T_ANDAND",		// &&
+	"T_DOT",		// .
+	"T_COMMA",		// ,
+	"T_CARET",		// ^
+	"T_QUES",		// ?
+	"T_EXCLAM",		// !
+	"T_GT",			// >
+	"T_LT",			// <
+	"T_HASHTAG",	// #
+	"T_BSLASH",
+	"T_TILDE",		// ~
+	"T_COLON",		// :
+	"T_SEMI",		// ;
+
+	"T_OP_CB",		// {
+	"T_CL_CB",		// }
+	"T_OBRKT",		// [
+	"T_CBRKT",		// ]
+	"T_OP",			// {
+	"T_CP",			// }
+
+	"T_INC",		// ++
+	"T_DEC",		// --
+
+	"T_ISEQUAL",	// ==
+	"T_INCEQUAL",	// +=
+	"T_DECEQUAL",	// -=
+	"T_MUEQUAL",	// *=
+	"T_MODEQUAL",	// %=
+	"T_SHLEQUAL",	// <<=
+	"T_SHREQUAL",	// >>=
+	"T_DIVEQUAL",	// /=
+	"T_NOTEQUAL",	// !=
+	"T_ANDEQUAL",	// &=
+	"T_OREQUAL",	// |=
+	"T_XOREQUAL",	// ^=
+	"T_SHIFT_L",	// <<
+	"T_SHIFT_R",	// >>
+
+	"T_DO",
+	"T_WHILE",
+	"T_FOR",
+	"T_IF",
+	"T_ELSE",
+	"T_ENUM",
+	"T_STRUCT",
+	"T_UNION",
+	"T_AUTO",
+	"T_BREAK",
+	"T_CONTINUE",
+	"T_DEFAULT",
+	"T_SWITCH",
+	"T_CASE",
+	"T_VOLATILE",
+	"T_CONST",
+	"T_SIGNED",
+	"T_UNSIGNED",
+	"T_STATIC",
+	"T_EXTERN",
+	"T_GOTO",
+	"T_RETURN",
+	"T_SIZEOF",
+	"T_REGISTER",
+	"T_TYPEDEF",
+	"T_LABEL",
+
+	"T_INT",
+	"T_CHAR",
+	"T_VOID",
+	"T_DOUBLE",
+	"T_FLOAT",
+	"T_LONG",
+
+	"T_STRLIT",		 // "string"
+	"T_INTLIT",		 // 123
+	"T_OCTALLIT",	 // 012
+	"T_HEXLIT",		 // 0x123
+	"T_BINLIT",		 //	0b123 
+	"T_CHARLIT",	 // 'c'
+
+	"T_IDENT",
+
+	"T_BAD"
+};
 
 #endif
