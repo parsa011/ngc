050f0fa0996e98d745c0c71bc05886ae551be73a
parsa011 <parsa32569@gmail.com>
2022-04-11 21:09:57 +0430

    [lexer.h] : Lexer Stuff declerations and definitions
    add more informations to lexer struct and some new functions for
    create and close lexers

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -6,6 +6,8 @@
 *		(C) 2022 Parsa Mahmoudy Sahebi
 *
 *	This file is part of ngc languge (new generation c)
+*	
+*	first phase of compiler
 */
 
 #include "../ngc.h"
@@ -15,9 +17,27 @@ struct lexer {
 	FILE *fp;
 	char *file_name;
 	char current_char;
+	/* contains current text of input file */
+	char *buf;
+	size_t buflen; // Length of buffer currently used
+	size_t alloc;  // Space allocated for buffer
+
 	struct position pos;
 	struct position prevpos;
+
 };
+
+/*
+ *	@brief : Init new lexer, open file by give name and return pointer to lexer
+ *		- name of file to open in lexer
+ */
+public struct lexer *lexer_init(char *);
+
+/*
+ *	@brief : Clsoe given lexer
+ *		- lexder to clsoe and free
+ */
+public void lexer_close(struct lexder *);
 
 /*
  *	@brief : Return new char from given file, but If There isn't Anything Saved Char
@@ -26,6 +46,7 @@ private char next_char();
 
 /*
  *	@brief : Put back given Char, To get it after new new_char call
+ *		- char to put back
  */
 private void put_back(int);
 
@@ -43,5 +64,11 @@ private void skip_ol_comment();
  *	@brief : Skipt Multiline comments
  */
 private void skip_ml_comment();
+
+/*
+ *	@brief : Return next token as pointer from give lexer
+ *		- lexer to read it file
+ */
+public struct token *lexer_next(struct lexer *);
 
 #endif
