7931314aab8d4c97a3330120be467a66e96a57e3
parsa011 <parsa32569@gmail.com>
2022-04-11 11:11:03 +0430

    [mem.c] : Some Basic Memory Functino
    for same memory allocation , we wrote some basic function.
    check allocated memory with assert to ensure that we got some memory :)

--- src/util/mem.c
+++ src/util/mem.c
@@ -6,3 +6,34 @@
 *	This file is part of ngc languge (new generation c)
 */
 
+#include "mem.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+void *ngc_malloc(size_t s)
+{
+	void *ptr = malloc(s);
+	assert(ptr);
+	return ptr;
+}
+
+void *ngc_calloc(size_t nmem, size_t s)
+{
+	void *ptr = calloc(nmem, s);
+	assert(ptr);
+	return ptr;
+}
+
+void *ngc_realloc(void *p, size_t s)
+{
+	void *ptr = realloc(p, s);
+	assert(ptr);
+	return ptr;
+}
+
+void ngc_free(void *ptr)
+{
+	assert(ptr);
+	free(ptr);
+}
--- src/util/mem.h
+++ src/util/mem.h
@@ -6,6 +6,30 @@
 *		(C) 2022 Parsa Mahmoudy Sahebi
 *
 *	This file is part of ngc languge (new generation c)
+*
+*	Memory-bound Utility function
 */
+
+#include <stddef.h>
+
+/*
+ *	@brief : malloc memory with given size
+ */
+void *ngc_malloc(size_t);
+
+/*
+ *	@brief : calloc, panic when error happened
+ */
+void *ngc_calloc(size_t, size_t);
+
+/*
+ *	@brief : relloc given pointer with new size
+ */
+void *ngc_realloc(void *, size_t);
+
+/*
+ *	@brief : just free, no more
+ */
+void ngc_free(void *);
 
 #endif
