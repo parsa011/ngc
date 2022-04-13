2fbbfd85ff551cb3b7ffa80d9ca8f80ae8855115
parsa011 <parsa32569@gmail.com>
2022-04-13 17:24:54 +0430

    [main.c] : simple test to write given file of lexer

--- src/main.c
+++ src/main.c
@@ -40,5 +40,19 @@ int main(int argc, char *argv[])
 	}
 	struct lexer *l = lexer_init();
 	l->open_file(l, argv[1]);
+	set_working_lexer(l);
+	lex(l);
+	printf("1 : ");
+	bool insert_line = false;
+	while (l->current_char != EOF) {
+		if (insert_line) {
+			printf("%d : ", l->pos.line);
+			insert_line = false;
+		}
+		if (l->current_char == '\n')
+			insert_line = true;
+		putchar(l->current_char);
+		lex(l);
+	}
 	return 0;
 }
