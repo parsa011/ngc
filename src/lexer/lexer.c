c7af8fc8997e78cdbb4f6b068129a247ebf1ecd4
parsa011 <parsa32569@gmail.com>
2022-04-13 17:24:06 +0430

    [lexer.c] : put_back and read_char functions

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -16,6 +16,8 @@ public struct lexer *lexer_init()
 	struct lexer *l = ngc_malloc(sizeof(struct lexer));
 	l->open_file = &lexer_open_file;
 	l->close_file = &lexer_close_file;
+	l->put_back_char = 0;
+	l->pos = new_pos_struct;
 	return l;
 }
 
@@ -29,9 +31,33 @@ public bool is_current_lexer(struct lexer *l)
 	return working_lexer == l;
 }
 
+private void put_back(char c)
+{
+	working_lexer->put_back_char = c;
+}
+
+private char next_char()
+{
+	char c;
+	char pt_back = get_lexer_put_back(working_lexer);
+	if (pt_back) {
+		c = pt_back;
+		put_back(0);
+		return c;
+	}
+	c = lexer_file_read_char(working_lexer->fp);
+	if (c == '\n') {
+		working_lexer->pos.line++;
+		working_lexer->pos.col = 0;
+	} else
+		working_lexer->pos.col++;
+	return c;
+}
+
 public void lex(struct lexer *l)
 {
 	if (!is_current_lexer(l)) {
 		panic("lexer->lex() : given lexer is not working lexer, lexer name : %s", l->file_name);
 	}
+	l->current_char = next_char();
 }
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -17,14 +17,18 @@ struct lexer {
 	FILE *fp;
 	char *file_name;
 	char current_char;
+	char put_back_char;
 
 	struct position pos;
 	struct position prevpos;
+
 
 	/* I/O functions */
 	void (*open_file)(struct lexer *, char *);
 	void (*close_file)(struct lexer *);
 };
+
+#define get_lexer_put_back(l) (l->put_back_char)
 
 /*
  *	pointer to lexer that we are working with
@@ -49,6 +53,8 @@ public bool is_current_lexer(struct lexer *l);
 
 /*
  *	@brief : lex given lexer and find new token of it
+ *	> make sure that you set target lexer and working_lexer by calling set_working_lexer before
+ *	  using this function, because it will panic if given lexer is not working_lexer
  */
 public void lex(struct lexer *l);
 
@@ -67,7 +73,7 @@ private char next_char();
  *	@brief : Put back given Char, To get it after new new_char call
  *		- char to put back
  */
-private void put_back(int);
+private void put_back(char);
 
 /*
  *	@brief : Skip White spaces And and put_back last non space char to give it from next_char
