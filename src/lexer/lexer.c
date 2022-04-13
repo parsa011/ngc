7be42602454e556b83a477509b4bb4312b03178b
parsa011 <parsa32569@gmail.com>
2022-04-13 16:11:15 +0430

    [lexer.h] : fill open and close pointers of lexer struct when init

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -7,9 +7,13 @@
 */
 
 #include "lexer.h"
+#include "lexer_file.h"
 
 public struct lexer *lexer_init()
 {
 	struct lexer *l = ngc_malloc(sizeof(struct lexer));
+	l->open_file = &lexer_open_file;
+	l->close_file = &lexer_close_file;
 	return l;
 }
+
